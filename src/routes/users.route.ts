fca4570825461089b2e626133b090af888ebd225
34anguyen <annguyen300243@gmail.com>
2025-07-16 01:02:59 +0700

    Fix: Add filter middleware to update user endpoint
    A
    Create filterMiddleware to the PATCH /users/me route to allow only specific fields in the request body. This enhances security by preventing unwanted fields from being updated.

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -11,6 +11,7 @@ import {
   verifyEmailController,
   verifyForgotPasswordController
 } from '~/controllers/users.controller'
+import { filterMiddleware } from '~/middlewares/common.middleware'
 import {
   accessTokenValidator,
   emailVerifyTokenValidator,
@@ -23,6 +24,7 @@ import {
   verifiedUserValidator,
   verifyForgotPasswordValidation
 } from '~/middlewares/users.middleware'
+import { UpdateMeReqBody } from '~/models/requests/User.request'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
@@ -97,6 +99,16 @@ usersRouter.patch(
   accessTokenValidator,
   verifiedUserValidator,
   updateMeValidator,
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'location',
+    'website',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   wrapRequestHandler(updateMeController)
 )
 
